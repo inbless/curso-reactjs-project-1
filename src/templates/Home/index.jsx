b866bc94c5766f99e952fd8e962d689bc1ce6df9
InBless Ltda <contato@inbless.com.br>
2024-04-17 09:47:45 -0300

    Alterado número de posts

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -12,7 +12,7 @@ export class Home extends Component {
         posts: [],
         allPosts: [],
         page: 0,
-        postsPerPage: 3,
+        postsPerPage: 10,
         searchValue: ''
     };
 
